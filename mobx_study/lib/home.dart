fe5e0dde9540daeaee13041c1c4387309f83f6c8
Andrey Gonçalves <49600701+zAndreyG@users.noreply.github.com>
2021-08-24 23:26:48 -0300

    First Commit - Counter with MobX

---
+++ mobx_study/lib/home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx_study/controller.dart';
+
+class MyHomePage extends StatelessWidget {
+  var controller = Controller();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MobX'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Observer(
+              builder: (_) {
+                return Text(
+                  '${controller.counter}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          controller.increment();
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
